750f918ee8ef2ad469ebcefde7969a0571cc524f
Vijay Vishwanathula <vijaykumar.vishwanathula@gmail.com>
2024-09-27 11:35:56 +0530

    codedeploy

---
+++ app/main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import langchain_groq as lc
+from langchain_community.document_loaders import WebBaseLoader
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
+
+
+def create_streamlit_app(llm, portfolio, clean_text):
+    st.title("📧 Cold Mail Generator")
+    url_input = st.text_input("Enter a URL:", value="https://www.naukri.com/job-listings-ai-ml-engineer-accenture-solutions-pvt-ltd-hyderabad-5-to-7-years-250924921228?src=jobsearchDesk&sid=17274160977562676&xp=1&px=1")
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs:
+                skills = job.get('skills', [])
+                links = portfolio.query_links(skills)
+                email = llm.write_mail(job, links)
+                st.code(email, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+if __name__ == "__main__":
+    chain = Chain()
+    portfolio = Portfolio()
+    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    create_streamlit_app(chain, portfolio, clean_text)
+
+
+
+
+
